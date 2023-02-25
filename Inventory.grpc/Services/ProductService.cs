fa82bb90c892cd726c622c3627a0fc7ab6a9be1e
Yael Chavez <you400@gmail.com>
2023-02-25 13:59:51 -0600

    orderin 1

--- Inventory.grpc/Services/ProductService.cs
+++ Inventory.grpc/Services/ProductService.cs
@@ -13,3 +13,37 @@ namespace Inventory.grpc.Services
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ * 1 el proyecto tiene que contar con una arquitectura limpia
+ * 2 se tiene que desarrollar con el DDD
+ * 3 usaremos el patron de mediador
+ * 4 tendremos que tener un motor de validaciones y de control de excepciones globales
+ * 5 usaremos el patron de repositorio pero haciendo uso de los generics para reutilizar el codigo
+ * 6 paginacion, ordenamiento, joins y CRUD
+ * 7 campos de auditoria
+ * 8 manejaremos multi tenanci
+ * 9 usar CQRS, AutoMapper, MediatR
+ * 10 pruebas unitarias
+ * */
---
+++ Ordering.Infrastructure/Persistence/OrderContext.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Common;
+using Ordering.Domain.Entities;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContext : DbContext
+    {
+
+        public OrderContext(DbContextOptions<OrderContext> contextOptions) : base(contextOptions)
+        {
+
+        }
+
+        public DbSet<Order> Orders { get; set; }
+        private Guid TenantId { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<EntityBase>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Modified:
+                        entry.Entity.CreatedBy = "User";
+                        entry.Entity.CreatedDate = DateTime.UtcNow;
+                        break;
+                    case EntityState.Added:
+                        entry.Entity.LastModifiedBy = "User";
+                        entry.Entity.LastModifiedDate = DateTime.UtcNow;
+                        break;
+                }
+            }
+
+            foreach (var item in ChangeTracker.Entries().Where(e=> e.State == EntityState.Added 
+            && e.Entity is IMultiTenant))
+            {
+                var entity = item.Entity as IMultiTenant;
+                entity!.TenantId = Guid.Empty;
+            }
+
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            foreach (var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                var entityType = entity.ClrType;
+
+                if (!typeof(IMultiTenant).IsAssignableFrom(entityType)) continue;
+
+                var method = typeof(OrderContext).GetMethod(nameof(MultiTenantExpression),
+                    BindingFlags.NonPublic | BindingFlags.Static)?.MakeGenericMethod(entityType);
+
+                var filter = method?.Invoke(null, new object[] { this });
+
+                entity.SetQueryFilter((LambdaExpression)filter!);
+
+                entity.AddIndex(entity.FindProperty(nameof(IMultiTenant.TenantId))!);
+
+            }
+        }
+
+        private static LambdaExpression MultiTenantExpression<T>(OrderContext context)
+            where T : EntityBase, IMultiTenant
+        {
+            Expression<Func<T, bool>> tenantFilter = x => x.TenantId == context.TenantId;
+            return tenantFilter;
+        }
+    }
+}
