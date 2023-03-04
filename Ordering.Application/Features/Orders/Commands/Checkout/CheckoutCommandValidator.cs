99dbda30b9836a96c9f2028201fc02090881cb7b
Yael Chavez <you400@gmail.com>
2023-03-04 12:52:11 -0600

    fluent validator 1

---
+++ Ordering.Application/Features/Orders/Commands/Checkout/CheckoutCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace Ordering.Application.Features.Orders.Commands.Checkout
+{
+    public class CheckoutCommandValidator : AbstractValidator<CheckoutCommand>
+    {
+        public CheckoutCommandValidator()
+        {
+            RuleFor(x => x.Address)
+             .EmailAddress()
+             .NotEmpty();
+
+            RuleFor(x => x.TotalPrice)
+                .GreaterThan(0);
+
+            RuleFor(x => x.UserName)
+                .MinimumLength(4)
+                .MaximumLength(20);
+        }
+    }
+}
---
+++ Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQueryValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    public class GetOrdersListQueryValidator: AbstractValidator<GetOrdersListQuery>
+    {
+        public GetOrdersListQueryValidator()
+        {
+            RuleFor(x => x.UserName)
+                .NotEmpty()
+                .MinimumLength(4)
+                .WithMessage("el username no puede ser menor que 4 caracteres")
+                .MaximumLength(20);
+        }
+
+    }
+}
